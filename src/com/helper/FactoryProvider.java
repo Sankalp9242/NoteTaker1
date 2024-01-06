527a230e6dd21fa95a6c654e0c8f2ce3d5e8d321
Sankalp9242 <sankalpsandugade@gmail.com>
2024-01-06 22:16:43 +0530

    Initial commit

---
+++ src/com/helper/FactoryProvider.java
@@ -0,0 +1,26 @@
+package com.helper;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.entities.Note;
+
+public class FactoryProvider {
+
+	public static SessionFactory factory;
+
+	public static SessionFactory getFactory() {
+
+		if (factory == null) {
+			factory = new Configuration().configure("noteTaker.cfg.xml").addAnnotatedClass(Note.class).buildSessionFactory();
+		}
+
+		return factory;
+	}
+
+	public void closeFactory() {
+		if (factory.isOpen()) {
+			factory.close();
+		}
+	}
+}
